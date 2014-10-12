4cc6c76efa89b55c474e200de036fce307507c48
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 16:17:38 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ -f /etc/configured ]; then
+        cd ~/catkin_ws/
+        source devel/setup.bash
+        echo 'already configured'
+else
+      #code that need to run only one time ....
+        
+        #needed for fix problem with ubuntu and cron
+        update-locale 
+        date > /etc/configured
+fi
