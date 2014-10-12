cac54bd24d7e7a7e3d850c6fa9939602c3a77b58
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 18:27:40 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,13 +3,11 @@
 set -e
 
 if [ -f /etc/configured ]; then
-        cd ~/catkin_ws/
-        source devel/setup.bash
         echo 'already configured'
 else
       #code that need to run only one time ....
-        cd ~/catkin_ws/
-        source devel/setup.bash
+        echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+        source ~/.bashrc
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
