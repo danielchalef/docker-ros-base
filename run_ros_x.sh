85950c82c5770d9bf2668ef6ae2a9160aac6d571
Daniel Chalef <daniel.chalef@gmail.com>
2015-04-29 22:11:00 -0400

    revised run script

--- run_ros_x.sh
+++ run_ros_x.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-xhost +
-docker run \
-   -p 11311:11311 -p 33433:33433 -p 80:80 -p 9090:9090 \
-   -v /tmp/.X11-unix:/tmp/.X11-unix \
-   -e DISPLAY=$DISPLAY \
-   -it ros-desktop-full /bin/bash
-xhost -
+docker run --name="ros-desktop" \
+	--privileged -i -v /tmp/.X11-unix/X0:/tmp/.X11-unix/X0 \
+	-v /dev/dri:/dev/dri -v /dev/shm:/dev/shm -e DISPLAY=:0.0 \
+	-t ros-indigo-desktop-full:latest
